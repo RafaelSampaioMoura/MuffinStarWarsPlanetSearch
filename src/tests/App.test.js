575cf90faae4c248224fe46a92474d10b804f80b
Rafael Sampaio Moura <rafaelsm123@outlook.com>
2022-10-27 11:47:49 -0300

    30% de Cobertura

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,9 +1,27 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { findAllByTestId, getByTestId, render, screen } from '@testing-library/react';
 import App from '../App';
+import userEvent from '@testing-library/user-event';
 
-test('I am your test', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Hello, App!/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('Testes de requisitos', () => {
+  jest.setTimeout(30000);
+  test('30%', async () => {
+    render(<App />);
+
+    const header = screen.getByTestId('name-filter');
+    // console.log(header);
+    const planetTable = await screen.findAllByTestId('planet-name', {}, { timeout: 10000});
+    const collumnInput = screen.getByTestId('column-filter');
+    const comparisonInput = screen.getByTestId('comparison-filter');
+    const valueInput = screen.getByTestId('value-filter');
+    const btnFilter = screen.getByTestId('button-filter');
+
+    expect(header).toBeInTheDocument();
+    // expect(planetTable).toBeInTheDocument();
+    expect(planetTable.length).toBe(10);
+    expect(collumnInput).toBeInTheDocument();
+    expect(comparisonInput).toBeInTheDocument();
+    expect(valueInput).toBeInTheDocument();
+    expect(btnFilter).toBeInTheDocument();
+  })
+})
